2a5dfe30205e6a73e8938dca8dea77a0fa8aeed4
rayhans358 <rayhans358@gmail.com>
2023-10-15 16:52:33 +0700

    Case Study

--- insight-server/app/controller/deliveryAddress.js
+++ insight-server/app/controller/deliveryAddress.js
@@ -37,56 +37,24 @@ const postDeliveryAddress = async (req, res, next) => {
     next(err);
   }
 }
-/*
-const putUpdateDeliveryAddress = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const payload = req.body;
-    let address = await DeliveryAddress.findById(id);
-    const subjectAddress = subject('DeliveryAddress', { ...address, user_id: address.user });
-    let policy = policyFor(req.user);
 
-    if (!policy.can('update', subjectAddress)) {
-      return res.json({
-        error: 1,
-        message: `You're not allowed to modify this resource`
-      });
-    }
-
-    address = await DeliveryAddress.findByIdAndUpdate(id, payload, { new: true });
-    res.json(address);
-  } catch (err) {
-    if (err && err.name === 'ValidationError') {
-      return res.json({
-        error: 1,
-        message: err.message,
-        fields: err.errors
-      });
-    }
-    next(err);
-  }
-}
-*/
 const putUpdateDeliveryAddress = async (req, res, next) => {
   
   try {
     let { id } = req.params;
     const payload = req.body;
     let address = await DeliveryAddress.findById(id);
-    // const policy = policyFor(req.user);
+    const policy = policyFor(req.user);
 
-    // if (!policy.can('update', 'DeliveryAddress')) {
-    //   return res.json({
-    //     error: 1,
-    //     message: 'You are not allowed to update DeliveryAddress'
-    //   });
-    // }
+    if (!policy.can('update', 'DeliveryAddress')) {
+      return res.json({
+        error: 1,
+        message: 'You are not allowed to update DeliveryAddress'
+      });
+    }
 
     address = await DeliveryAddress.findByIdAndUpdate(id, payload, { new: true }
     );
-
-    // await address.save();
-    console.log(address, '<<< address');
     return res.json(address);
 
   } catch (err) {
