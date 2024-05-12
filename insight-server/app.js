46adb28587c524a3c039181690a0eb6f7ecb6b6e
rayhans358 <rayhans358@gmail.com>
2024-05-12 22:07:26 +0700

    Deploy Vercel

--- insight-server/app.js
+++ insight-server/app.js
@@ -13,7 +13,13 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(cors());
+app.use(cors(
+  {
+    origin: ["https://deploy-mern-frontend.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true
+  }
+));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
