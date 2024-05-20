d2a896075fa8bd74a6d791998eac5b452edd10bf
rayhans358 <rayhans358@gmail.com>
2024-05-20 15:07:21 +0700

    Deploy Vercel

--- insight-server/app.js
+++ insight-server/app.js
@@ -13,13 +13,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(cors(
-  {
-    origin: ["https://insight-coffee.vercel.app"],
-    methods: ["GET", "POST", "CREATE", "UPDATE"],
-    credentials: true
-  }
-));
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
