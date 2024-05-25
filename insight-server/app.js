7dc92a56c08c92c180625b570c4ce690d7ca1027
rayhans358 <rayhans358@gmail.com>
2024-05-25 14:22:15 +0700

    Deploy Vercel

--- insight-server/app.js
+++ insight-server/app.js
@@ -1,48 +1,56 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const cors = require('cors');
-const indexRouter = require('./app/router/index');
-const {decodeToken} = require('./app/middleware/passport-jwt-strategy');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const cors = require("cors");
+const indexRouter = require("./app/router/index");
+const { decodeToken } = require("./app/middleware/passport-jwt-strategy");
+
+// Manual import check for base64-vlq
+try {
+  const base64VLQ = require("source-map/lib/base64-vlq");
+  console.log("base64-vlq module loaded successfully");
+} catch (err) {
+  console.error("Failed to load base64-vlq module:", err);
+}
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
 app.use(cors());
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(decodeToken());
 
 app.use(indexRouter);
 //home
-app.use('/', function(req, res) {
-  res.render('index', {
-    title: "Insight Store"
+app.use("/", function (req, res) {
+  res.render("index", {
+    title: "Insight Store",
   });
 });
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
