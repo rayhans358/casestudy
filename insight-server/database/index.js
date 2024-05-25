79a98175de76a6bd6c3a144a01c16c13f6918fd8
rayhans358 <rayhans358@gmail.com>
2024-05-25 16:00:28 +0700

    Deploy Vercel

--- insight-server/database/index.js
+++ insight-server/database/index.js
@@ -1,7 +1,9 @@
-const mongoose = require('mongoose');
-const{dbUser, dbPass, dbHost, dbName} = require('../app/config');
+const mongoose = require("mongoose");
+const { dbUser, dbPass, dbHost, dbName } = require("../root/config");
 
-mongoose.connect(`mongodb+srv://${dbUser}:${dbPass}@${dbHost}/${dbName}?retryWrites=true&w=majority`);
+mongoose.connect(
+  `mongodb+srv://${dbUser}:${dbPass}@${dbHost}/${dbName}?retryWrites=true&w=majority`
+);
 
 const db = mongoose.connection;
 
