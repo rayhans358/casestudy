06c33f63f03db17ec875d5ee1d22b452c8482f27
rayhans358 <rayhans358@gmail.com>
2024-05-25 22:11:53 +0700

    Deploy Vercel

--- insight-server/database/index.js
+++ insight-server/database/index.js
@@ -1,9 +1,21 @@
 const mongoose = require("mongoose");
 const { dbUser, dbPass, dbHost, dbName } = require("../app/config");
 
-mongoose.connect(
-  `mongodb+srv://${dbUser}:${dbPass}@${dbHost}/${dbName}?retryWrites=true&w=majority`
-);
+mongoose
+  .connect(
+    `mongodb+srv://${dbUser}:${dbPass}@${dbHost}/${dbName}?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 500000,
+    }
+  )
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB", err);
+  });
 
 const db = mongoose.connection;
 
